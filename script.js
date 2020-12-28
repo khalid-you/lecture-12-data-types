e69ff05ef57bcf8c41db26a789b41923f08adc49
admin <khalidyoy240@gmail.com>
2020-12-28 05:27:20 -0800

    This is javascript data types

---
+++ script.js
@@ -0,0 +1,36 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log('Khalid');
+console.log(14);
+let firstName = "Khalid Saleh";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "student";
+let myCurrentJob = "I'm still a student";
+console.log(myFirstJob);
+console.log(myCurrentJob);
+*/
+
+// The data types in JavaScript
+let javaScriptIsfun = true;
+console.log(typeof javaScriptIsfun);
+console.log(typeof true);
+
+console.log(typeof false);
+console.log(typeof javaScriptIsfun);
+console.log(typeof 'Khalid');
+console.log(typeof 23);
+
+javaScriptIsfun = "Not all the time!!";
+console.log(typeof javaScriptIsfun);
+
+let year;
+console.log(typeof year);
+
+year = 2020;
+console.log(typeof year);
+
+console.log(typeof null);
